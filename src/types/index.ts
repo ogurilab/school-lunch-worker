b9f8b8aae5e4ec4a346c252b44d4c10a949a80cc
shouta0715 <Myheroes0715@gmail.com>
2024-02-09 14:23:44 +0900

    fix: types and utils

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,26 @@
 export type Env = {
   BUCKET: R2Bucket;
   WEBHOOK_URL: string;
+  R2_URL: string;
+};
+
+type Embed = {
+  title: string;
+  type: "rich" | "image";
+  description?: string;
+  color?: number;
+  image?: {
+    url: string;
+  };
+};
+
+export type DiscordMessage = {
+  content: string;
+  embeds?: Embed[];
+};
+
+export type OnDiscordNotificationProps = {
+  env: Env;
+  error?: unknown;
+  url?: string;
 };
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,32 +1,31 @@
 import { load } from "cheerio";
-import { BASE_URL, HTML_URL } from "@/constant/handa";
 
-export function getTodayYYYYMMDD() {
+import { BASE_URL, HTML_URL } from "@/constant";
+import { ScrapingError } from "@/errors";
+
+export function getTodayDate(format: "YYYY年MM月DD日" | "YYYYMMDD") {
   const date = new Date();
   const year = date.getFullYear();
   const month = date.getMonth() + 1;
   const day = date.getDate();
 
-  return `${year}${month}${day}`;
-}
-
-export function getTodayDate() {
-  const date = new Date();
-  const year = date.getFullYear();
-  const month = date.getMonth() + 1;
-  const day = date.getDate();
+  if (format === "YYYYMMDD") {
+    return `${year}${month}${day}`;
+  }
 
   return `${year}年${month}月${day}日`;
 }
 
 function getAlt() {
-  const today = getTodayDate();
+  const today = getTodayDate("YYYY年MM月DD日");
 
   return `${today}献立`;
 }
 
 async function getImageSrc() {
   const res = await fetch(HTML_URL).then((r) => r.text());
+  if (!res) throw new ScrapingError();
+
   const $ = load(res);
 
   const alt = getAlt();
@@ -40,13 +39,13 @@ async function getImageSrc() {
 export async function getImage() {
   const imageSrc = await getImageSrc();
 
-  if (!imageSrc) throw new Error("Image URL not found");
+  if (!imageSrc) throw new ScrapingError();
 
   const imageUrl = `${BASE_URL}${imageSrc}`;
 
   const res = await fetch(imageUrl);
 
-  if (!res.ok) throw new Error("Image not found");
+  if (!res.ok) throw new ScrapingError();
 
   return res;
 }
